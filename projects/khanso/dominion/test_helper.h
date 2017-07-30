0e11ce01b65d3fa4ed9380637755cd1111b498dc
sohailkhanosu <khanso@oregonstate.edu>
2017-07-29 22:19:10 -0700

    add not equal macro to testing harness

--- projects/khanso/dominion/test_helper.h
+++ projects/khanso/dominion/test_helper.h
@@ -5,14 +5,15 @@
 #ifndef _TEST_HELPER_H
 #define _TEST_HELPER_H
 
+#include <stdio.h>
+
 unsigned int TESTS_PASSED = 0;
 unsigned int TESTS_FAILED = 0;
 
-#define FAILED_TEST(_name_, _expected_, _actual_) do {\
-    fprintf(stderr, "failed: %s: expected %d to be %d\n", (_name_), (_actual_), (_expected_));\
+#define FAILED_TEST(...) do {\
+    fprintf(stderr, __VA_ARGS__); \
     TESTS_FAILED++;\
 } while (0)
-
 
 #define PASSED_TEST do { \
     TESTS_PASSED++; \
@@ -22,9 +23,18 @@ unsigned int TESTS_FAILED = 0;
     if ((_expected_) == (_actual_)) {\
         PASSED_TEST; \
     } else  { \
-        FAILED_TEST((_name_), (_expected_), (_actual_));\
+        FAILED_TEST("failed: %s: expected %d to be %d\n", (_name_), (_expected_), (_actual_));\
     }\
 } while(0)
+
+#define EXPECT_NOT_EQUAL(_name_, _expected_, _actual_) do{\
+    if ((_expected_) != (_actual_)) {\
+        PASSED_TEST; \
+    } else { \
+        FAILED_TEST("failed: %s: expected %d to not be %d\n", (_name_), (_expected_), (_actual_)); \
+    }\
+} while(0)
+
 
 #define SUMMARY do {\
     fprintf(stdout, "TESTS_PASSED: %d TESTS_FAILED: %d\n", TESTS_PASSED, TESTS_FAILED);\
