3398043a9f4306b759d969a47bc062d065809518
sohailkhanosu <khanso@oregonstate.edu>
2017-08-02 21:07:31 -0700

    fix adventurer bug that gave more cards than necessary

--- projects/khanso/dominion/dominion.c
+++ projects/khanso/dominion/dominion.c
@@ -670,10 +670,14 @@ int adventurerEffect(int currentPlayer, int handPos, struct gameState *state)
   while(drawntreasure<=2){
       if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
           shuffle(currentPlayer, state);
+          if (state->deckCount[currentPlayer] < 1) {
+            // player has zero cards and should return -1
+            return -1;
+          }
       }
       drawCard(currentPlayer, state);
       cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-      if (cardDrawn = copper || cardDrawn == silver || cardDrawn == gold)
+      if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
           drawntreasure++;
       else{
           temphand[z]=cardDrawn;
